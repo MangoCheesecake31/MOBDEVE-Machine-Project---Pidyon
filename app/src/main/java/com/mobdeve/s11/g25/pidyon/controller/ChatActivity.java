3063e61f4205656276cc2ad0ebf41e04591f9884
Maxson Chan <roymaxsonchanli98@gmail.com>
2021-08-24 03:58:25 +0800

    Added Progress Bar and Tabs in Main Activity, implemented Chat Activity

---
+++ app/src/main/java/com/mobdeve/s11/g25/pidyon/controller/ChatActivity.java
@@ -0,0 +1,16 @@
+package com.mobdeve.s11.g25.pidyon.controller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mobdeve.s11.g25.pidyon.R;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
--- app/src/main/java/com/mobdeve/s11/g25/pidyon/controller/MainActivity.java
+++ app/src/main/java/com/mobdeve/s11/g25/pidyon/controller/MainActivity.java
@@ -1,21 +1,38 @@
 package com.mobdeve.s11.g25.pidyon.controller;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 import android.util.Log;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.mobdeve.s11.g25.pidyon.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ViewPager2 viewPager;
+    private TabLayout tabLayout;
+
+    private FragmentAdapter fragmentAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewPager = findViewById(R.id.viewPager);
+        tabLayout = findViewById(R.id.tabLayout);
+
+        FragmentManager fm = getSupportFragmentManager();
+        fragmentAdapter = new FragmentAdapter(fm, getLifecycle());
+        viewPager.setAdapter(fragmentAdapter);
 
 //        TextView tv = findViewById(R.id.textName);
 
@@ -33,5 +50,30 @@ public class MainActivity extends AppCompatActivity {
                 Log.d("TEST", token);
             }
         });
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        viewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                tabLayout.selectTab(tabLayout.getTabAt(position));
+            }
+        });
+
     }
 }
